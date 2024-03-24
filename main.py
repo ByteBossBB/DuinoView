336edf8b08d7fca1225e22ecb3e78bc857f7e1a3
ByteBossBB <164728871+ByteBossBB@users.noreply.github.com>
2024-03-24 09:49:55 -0300

    Better interface

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ Description: This script displays a user's Duino-Coin account data., This script
 import requests
 import time
 import os
+from colorama import Fore, Style
+
+
 
 def clear_screen():
     # Comando para limpiar la pantalla en diferentes sistemas operativos
@@ -29,31 +32,36 @@ def main():
     while True:
         data = get_duco_data(wallet_address)
         if data:
-            print("---------------------------------------------------------------")
-            print(f"Username: {data['result']['balance']['username']}")
-            print(f"Balance: {data['result']['balance']['balance']} DUCOs")
-            print(f"Total Transactions: {len(data['result']['transactions'])}")
-            print(f"Total Miners: {len(data['result']['miners'])}")
-            print(f"Trust Score: {data['result']['balance']['trust_score']}")
-            print("---------------------------------------------------------------")
+            
+            
+            print("***************************************************************")
+            print("Miners:")
+            for i in range(len(data['result']['miners'])):
+                print("==============================================================================================")
+                print(Fore.BLUE + "Miner name: " + Style.RESET_ALL + f"{data['result']['miners'][i]['identifier']}")
+                print(Fore.YELLOW + "Hashrate: " + Style.RESET_ALL + F"{data['result']['miners'][i]['hashrate']}")                  
+                print(Fore.MAGENTA +"Diff: " + Style.RESET_ALL + f"{data['result']['miners'][i]['diff']}")                 
+                print(Fore.CYAN + "Software: " + Style.RESET_ALL + f"{data['result']['miners'][i]['software']}")
+                print("==============================================================================================")  
+            print("***************************************************************")
             print("###############################################################")
             print("Transactions:")
             for i in range(len(data['result']['transactions'])):
                 print("........................................................")
-                print(f"Sender: {data['result']['transactions'][i]['sender']}")
-                print(f"Amount: {data['result']['transactions'][i]['amount']}")
-                print(f"Data Time: {data['result']['transactions'][i]['datetime']}")
-                print(f"Memo: {data['result']['transactions'][i]['memo']}")
+                print(Fore.GREEN + "Sender: " + Style.RESET_ALL + f"{data['result']['transactions'][i]['sender']}")
+                print(Fore.YELLOW + "Amount: " + Style.RESET_ALL +  f"{data['result']['transactions'][i]['amount']}")
+                print(Fore.CYAN + "Data Time: " + Style.RESET_ALL + f"{data['result']['transactions'][i]['datetime']}")
+                print(Fore.MAGENTA + "Memo: "  + Style.RESET_ALL +  f"{data['result']['transactions'][i]['memo']}")
                 print("........................................................")
             print("###############################################################")
-            print("***************************************************************")
-            print("Miners:")
-            for i in range(len(data['result']['miners'])):
-                print(f"Miner name: {data['result']['miners'][i]['identifier']}")
-                print(f"Hashrate: {data['result']['miners'][i]['hashrate']}")
-                print(f"Diff: {data['result']['miners'][i]['diff']}")
-                print(f"Software: {data['result']['miners'][i]['software']}")
-            print("***************************************************************")
+            print("---------------------------------------------------------------")
+            print(Fore.CYAN + "Username: " + Style.RESET_ALL + f"{data['result']['balance']['username']}")
+            print(Fore.GREEN + "Balance: "  + Style.RESET_ALL +  f"{data['result']['balance']['balance']} DUCOs")
+            print(Fore.YELLOW + "Total Transactions: "  + Style.RESET_ALL + f"{len(data['result']['transactions'])}")
+            print(Fore.BLUE + "Total Miners: " + Style.RESET_ALL + f"{len(data['result']['miners'])}")
+            print(Fore.MAGENTA + "Trust Score: " + Style.RESET_ALL + f"{data['result']['balance']['trust_score']}")
+            print("---------------------------------------------------------------")
+            
         time.sleep(tiempo)
         clear_screen()
         
